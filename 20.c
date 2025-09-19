3f68db36e0572731912ee438a0471f40341b6660
kp-0705 <kartavya0705@gmail.com>
2025-09-19 16:01:44 +0530

    Update 20.c

--- 20.c
+++ 20.c
@@ -7,7 +7,24 @@ Description : 20. Find out the priority of your running program. Modify the prio
 Date :22th Aug, 2025
 ========================================================================================================================================================================================
 */
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/resource.h>
+#include <string.h>
+int main()
+{
+	int newp;
+	printf("\nEnter New priority : ");
+	scanf(" %d",&newp);
+	
+	int p = getpriority(PRIO_PROCESS , getpid());
+	printf("\nCurrent priority is %d.\n",p);
+	printf("\nAfter update : %d.\n",nice(newp));
+	return 0;
+}
 
 
 /*
